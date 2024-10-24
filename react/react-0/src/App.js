da4f570aee73589ebd795accb6bd6b6ac28c0a3d
WhaleSC12 <WJHALE@email.sc.edu>
2024-10-23 21:21:11 -0400

    dsfesddd

---
+++ react/react-0/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return <h1>The LORD was with the men of Judah. <br></br>
+  They took possession of the hill country, <br></br>
+  but they were unable to drive the people <br></br>
+  from the plains, because they had chariots<br></br>
+  fitted with iron.
+</h1>;
+}
+
+export default App;
